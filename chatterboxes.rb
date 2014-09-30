07eb3e4a537526de50a3e486e1e27105740f784c
Dan Murphy <murphydbuffalo@gmail.com>
2014-09-30 16:26:24 -0400

    Added Pony gem and Mandrill add-on for the email form on the homepage. Set up .gitignore and .env files.

--- chatterboxes.rb
+++ chatterboxes.rb
@@ -1,11 +1,43 @@
 require 'sinatra'
+require 'dotenv'
+require 'pony'
+Dotenv.load
+
+def mail_to(recipient, name, email, phone)
+  Pony.mail({
+    to: recipient,
+    from: "Web-Services@teamchatterboxes.com",
+    subject: "Someone is interested in an appointment at Chatterboxes!",
+    html_body: erb(:email),
+    body: "Hey Brittany,\n\t#{name} is interested in Chatterboxes!  They can be reached via email at #{email} or by phone at #{phone}.",
+    via: :smtp,
+    via_options: {
+      :address        => 'smtp.mandrillapp.com',
+      :port           => '587',
+      :user_name      => ENV['MANDRILL_USERNAME'],
+      :password       => ENV['MANDRILL_APIKEY'],
+      :authentication => :plain, 
+      :domain         => "heroku.com"
+    }
+  })
+end
 
 get '/' do
-  erb :home, layout: :application
+  redirect '/home'
 end
 
 get '/home' do
   erb :home, layout: :application
+end
+
+post '/home' do
+  @full_name = "#{params[:first_name].capitalize} #{params[:last_name].capitalize}"
+  @phone = params[:phone]
+  @email = params[:email]
+
+  mail_to("murphydbuffalo@gmail.com", @full_name, @email, @phone)
+
+  redirect '/home'
 end
 
 get '/about' do
